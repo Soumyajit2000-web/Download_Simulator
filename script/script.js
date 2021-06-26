405eabc5ab613b89fce266acd6ef08334d59048f
Soumyajit2000-web <84496904+Soumyajit2000-web@users.noreply.github.com>
2021-06-26 18:26:45 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -123,7 +123,7 @@ function calc(){
                 clearInterval(interval);
                 btn.disabled = false;
                 remainingTime.innerHTML = '0 seconds'
-                alert("Download Complete");
+               // alert("Download Complete");
             }
 
         }, 1000);
